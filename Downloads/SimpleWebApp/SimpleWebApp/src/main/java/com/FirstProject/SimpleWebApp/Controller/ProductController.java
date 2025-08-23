44ff7fc8a0fe01c64655bc1cec3d21f2eb8588f6
Shiwang Solanki <shiwangsolanki27@gmail.com>
2025-08-23 19:18:39 +0530

    CRUD application spring backend project completed

--- Downloads/SimpleWebApp/SimpleWebApp/src/main/java/com/FirstProject/SimpleWebApp/Controller/ProductController.java
+++ Downloads/SimpleWebApp/SimpleWebApp/src/main/java/com/FirstProject/SimpleWebApp/Controller/ProductController.java
@@ -3,9 +3,11 @@ package com.FirstProject.SimpleWebApp.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +39,15 @@ public class ProductController {
         // The @RequestBody annotation tells Spring Boot to take the JSON from
         // the request body and convert it into a Product object.
         service.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
--- Downloads/SimpleWebApp/SimpleWebApp/src/main/java/com/FirstProject/SimpleWebApp/Service/ProductService.java
+++ Downloads/SimpleWebApp/SimpleWebApp/src/main/java/com/FirstProject/SimpleWebApp/Service/ProductService.java
@@ -48,4 +48,23 @@ public class ProductService {
     public void addProduct(Product prod) {
         products.add(prod);
     }
+
+    public void updateProduct(Product prod) {
+        int index=0;
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId()== prod.getProdId())
+                index= i ;
+
+        products.set(index,prod);
+    }
+
+    public void deleteProduct(int prodId) {
+        int index=0;
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId()== prodId)
+                index= i ;
+
+        
+        products.remove(index);
+    }
 }
